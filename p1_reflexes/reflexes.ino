92ddd30b300677ff28a3347a01ec6aea6fc876c4
nonari <nonari@users.noreply.github.com>
2019-09-30 11:55:59 +0200

    Update reflexes.ino

--- p1_reflexes/reflexes.ino
+++ p1_reflexes/reflexes.ino
@@ -1,52 +1,73 @@
 const int button = 3;
 const int red_led = 10;
 const int green_led = 5;
-const int debounce_time = 50;
+
+const unsigned int debounce_time = 60;
+unsigned int last_bounce = millis();
+
+const String bad = "ms Intente de nuevo";
+const String good = "ms BIEN! Buenos reflejos";
+const String excelent = "ms ENHORABUENA! Excelentes reflejos";
+const String too_fast = "NO SE PRECIPITE. Intente de nuevo";
 
 void setup() {
-    randomSeed(54155);
+    randomSeed(analogRead(0));
     Serial.begin(115200);
     pinMode(button, INPUT);
     pinMode(red_led, OUTPUT);
     pinMode(green_led, OUTPUT);
 }
 
-boolean pressedButton() {
-    boolean pressed = false;
-    static unsigned long starttime = millis(); 
-    if (digitalRead(button)) {
-        if (millis() - starttime > debounce_time) {
-            pressed = true;
+void pressedButton() {
+    while (true) {
+        if (digitalRead(button)) {
+            unsigned int since_bounce = millis() - last_bounce;
+            if (since_bounce < debounce_time) {
+                delay(debounce_time - since_bounce);
+            } else {
+                break;
+            }
         }
-        starttime = millis();
     }
-    return pressed;
+    last_bounce = millis(); 
+}
+
+boolean isPressed() {
+    if (millis() - last_bounce > debounce_time) {
+        if (digitalRead(button)) {
+            return true;
+        } 
+    }
+    return false;
 }
 
 void unpressedButton() {
     while (true) {
         if (!digitalRead(button)) {
-           delay(debounce_time);
-           if (!digitalRead(button)) {
-               break;
-           }
+            unsigned int since_bounce = millis() - last_bounce;
+            if (since_bounce < debounce_time) {
+                delay(debounce_time - since_bounce);
+            } else {
+                break;
+            }
         }
     }
+    last_bounce = millis();
 }
 
 void loop() {
     Serial.println("Start");
     digitalWrite(green_led, HIGH);
     digitalWrite(red_led, LOW);
-    while (!pressedButton());
-    while (digitalRead(button));
+    pressedButton();
+    unpressedButton();
     int counter = random(400, 1200);
     digitalWrite(green_led, LOW);
     boolean fail = false;
     while (counter != 0 && !fail) {
         delay(1);
-        if (pressedButton()) {
-            Serial.println("Demasiado rapido");
+        if (isPressed()) {
+            Serial.println();
             fail = true;
         }
         counter--;
@@ -54,15 +75,16 @@ void loop() {
     if (!fail) {
         digitalWrite(red_led, HIGH);
         unsigned int time = millis();
-        while(!pressedButton());
+        pressedButton();
         digitalWrite(red_led, LOW);
-        unsigned int total = millis() - time;
+        int total = millis() - time;
         if (total < 100) {
-            Serial.print("Buenos reflejos");
+            Serial.println(total + excelent);
+        } else if (total < 200) {
+            Serial.println(total + good);
         } else {
-            Serial.print("Conduces como una ancianita");
+            Serial.println(total + bad);
         }
     }
-    while (digitalRead(button));
-    loop();
+    unpressedButton();
 }
